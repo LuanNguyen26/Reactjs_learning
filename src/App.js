00efc8d3ac10323afdbee205ae1387399c208a58
luanpro0780 <71505099+luanpro0780@users.noreply.github.com>
2021-03-17 10:53:15 +0700

    17-3

--- src/App.js
+++ src/App.js
@@ -33,5 +33,21 @@ const App = () => {
     </ThemeProvider>
   );
 };
+router.post('/', upload.single("upload_file_input"), (req, res) => {
+  var tempPath = req.file.path;
+  //const targetPath = path.join(__dirname, "./uploads/image.png");
+  var pathSave = path.join(__dirname, '../public/images/upload/image.jpg');
+
+  console.log(tempPath);
+
+  try{
+      fs.renameSync(tempPath, pathSave);        
+  }
+  catch(err){
+      console.log(err);
+  }
+
+  res.send('send file successfull ' + pathSave);
+});
 
 export default App;
